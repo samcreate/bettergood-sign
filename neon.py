2d7f0fa90f21abd88c403e9c7699d07a0304d3a0
Aaron McGuire <samuel.aaron.mcguire@gmail.com>
2019-04-15 14:01:46 -0700

    first commit

---
+++ neon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import time
+
+import automationhat
+
+
+if automationhat.is_automation_hat():
+    automationhat.light.power.write(1)
+
+while True:
+    automationhat.relay.one.toggle()
+    if automationhat.is_automation_hat():
+        automationhat.relay.two.toggle()
+        automationhat.relay.three.toggle()
+    time.sleep(0.1)

c1a9c0fc187d3e0557a0ec635d7b46aec4731666
Aaron McGuire <samuel.aaron.mcguire@gmail.com>
2019-04-15 14:41:12 -0700

    adding a function with sound

--- neon.py
+++ neon.py
@@ -6,13 +6,33 @@ import time
 
 import automationhat
 
+mixer.init()
 
-
+buzz = mixer.Sound('buzz.mp3')
 
 if automationhat.is_automation_hat():
     automationhat.light.power.write(1)
 
-while True:
-    automationhat.relay.one.toggle()
+# while True:
+#     automationhat.relay.one.toggle()
+    
+#     time.sleep(0.9)
 
+
+def buzz_lights():
+    print("Light Buzz begins")
+    buzz.play()
+    automationhat.relay.one.toggle()
+    time.sleep(0.01)
+    automationhat.relay.one.toggle()
+    time.sleep(0.41)
+    automationhat.relay.one.toggle()
+    time.sleep(0.41)
+    automationhat.relay.one.toggle()
     time.sleep(0.9)
+    utomationhat.relay.one.toggle()
+    time.sleep(0.9)
+    print("Light Buzz ends")
+
+
+buzz_lights()
